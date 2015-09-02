f4ea0d24deb32491a90e4ac55be6a68a8a8281f6
eunfon <eunfon0704@gmail.com>
2015-09-02 23:18:25 +0700

    test ka

---
+++ FinalPro/Assets/Script/Credit.js
@@ -0,0 +1,29 @@
+﻿#pragma strict 
+var guiGame : Texture; 
+
+var guiRight : Texture; 
+var guiLeft : Texture;
+
+var guiRightB : GUIStyle; 
+var guiLeftB : GUIStyle; 
+
+
+function Start () { 
+
+} 
+
+function Update () { 
+
+} 
+
+
+function OnGUI () { 
+GUI.DrawTexture (Rect(0,0,Screen.width,Screen.height),guiGame); 
+	//if(GUI.Button(Rect(100,600,100,80),guiRight,guiRightB)) {
+	//	Application.LoadLevel ("Credit2");
+//}	
+if(GUI.Button(Rect(0,600,100,80),guiLeft,guiLeftB)) { 
+		Application.LoadLevel ("first");
+} 	
+}
+
---
+++ FinalPro/Assets/Script/First.js
@@ -0,0 +1,28 @@
+﻿#pragma strict 
+var guiGame : Texture; 
+var guiStart : Texture; 
+var guiSetting : Texture;
+var guiCredit : Texture; 
+var guiStartB : GUIStyle; 
+var guiSettingB : GUIStyle; 
+var guiCreditB : GUIStyle; 
+
+function Start () { 
+
+} 
+
+function Update () { 
+
+} 
+
+
+function OnGUI () { 
+//GUI.DrawTexture /*(Rect (0,0,1024,768),guiGame)*/; 
+	if(GUI.Button(Rect(400,400,200,150),guiStart,guiStartB)) {
+		Application.LoadLevel ("LV1");
+}	if(GUI.Button(Rect(650,400,200,150),guiSetting,guiSettingB)) { 
+		Application.LoadLevel ("Setting");
+} 	if(GUI.Button(Rect(150,400,200,150),guiCredit,guiCreditB)) { 
+		Application.LoadLevel ("Credit"); } 
+}
+
