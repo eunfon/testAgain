1e47e94ce8b364e009aa9431b62a0387eebe3308
eunfon <eunfon0704@gmail.com>
2015-09-09 23:51:43 +0700

    add project test Coroutines

---
+++ eunfonCon/Assets/Loader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (mainWait());
+
+
+	//StartCoroutine (waitToShow (1f));
+}
+	// Update is called once per frame
+	void Update () {
+
+	}
+	IEnumerator mainWait(){
+		Debug.Log ("mainWait Start");
+		yield return new WaitForSeconds (3f);
+		Destroy (gameObject);
+		Debug.Log ("done Main done");
+//		yield return StartCoroutine (waitToShow (3f));
+		Debug.Log ("mainWait Finish");
+}
+
+}
---
+++ eunfonCon/Assets/Loader2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (mainWait());
+		
+		
+		//StartCoroutine (waitToShow (1f));
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator mainWait(){
+		Debug.Log ("mainWait Start");
+		yield return new WaitForSeconds (1f);
+		Destroy (gameObject);
+		Debug.Log ("done Main done");
+		//		yield return StartCoroutine (waitToShow (3f));
+		Debug.Log ("mainWait Finish");
+	}
+	
+}
